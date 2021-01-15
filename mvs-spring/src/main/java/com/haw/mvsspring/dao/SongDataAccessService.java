0cb081638e27439473b8fd61b1c9e6d63747004c
Parisa Ostadzadeh <parisa.ostadzadeh92@gmail.com>
2021-01-15 20:47:18 +0100

    Start implementing song search api
    As a user when I pass a song parameter(e.g. genre),
    I want to get an array of matched songs

--- mvs-spring/src/main/java/com/haw/mvsspring/dao/SongDataAccessService.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/dao/SongDataAccessService.java
@@ -5,10 +5,12 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.haw.mvsspring.Exceptions.MyDatabaseException;
 import com.haw.mvsspring.Exceptions.WrongDataException;
 import com.haw.mvsspring.model.Song;
+import com.haw.mvsspring.sqlite.SongRowMapper;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.UnsupportedTagException;
 
@@ -24,15 +26,15 @@ public class SongDataAccessService implements SongDao {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public SongDataAccessService(JdbcTemplate jdbcTemplate){
+    public SongDataAccessService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public int insertSong(Song song) {
         final String sql = "INSERT INTO song (filename, title, artist, genre, album, duration, album_image) VALUES(?, ?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, new Object [] { song.getFilename(), song.getTitle(), song.getArtist(), song.getGenre(),
-        song.getAlbum(), song.getDuration(), song.getAlbumImage()} );
+        jdbcTemplate.update(sql, new Object[] { song.getFilename(), song.getTitle(), song.getArtist(), song.getGenre(),
+                song.getAlbum(), song.getDuration(), song.getAlbumImage() });
         return 0;
     }
 
@@ -59,7 +61,7 @@ public class SongDataAccessService implements SongDao {
         jdbcTemplate.execute(sql);
         return 0;
     }
-    
+
     private boolean exists(final int id) {
         final String sql = "SELECT COUNT(*) FROM song WHERE id=" + id;
         final int count = jdbcTemplate.queryForObject(sql, Integer.class);
@@ -101,5 +103,43 @@ public class SongDataAccessService implements SongDao {
         }
         return 0;
     }
-    
+
+    public List<Song> searchInDB(final Map<String, String> searchItems) {
+
+        final var keySet = searchItems.keySet();
+        final String key = (String) keySet.toArray()[0];
+        final String value = searchItems.get(key);
+
+        String sql;
+        List<Song> result = new ArrayList<>();
+        switch (key) {
+            case "artist":
+                sql = "SELECT * FROM song WHERE artist = ? ";
+                result = jdbcTemplate.query(sql, new Object[] { value }, new SongRowMapper());
+                break;
+            case "album":
+                sql = "SELECT * FROM song WHERE album= ? ";
+                result = jdbcTemplate.query(sql, new Object[] { value }, new SongRowMapper());
+                break;
+            case "duration":
+                sql = "SELECT * FROM song WHERE duration = ? ";
+                result = jdbcTemplate.query(sql, new Object[] { value }, new SongRowMapper());
+                break;
+            case "filename":
+                sql = "SELECT * FROM song WHERE filename = ? ";
+                result = jdbcTemplate.query(sql, new Object[] { value }, new SongRowMapper());
+                break;
+            case "genre":
+                sql = "SELECT * FROM song WHERE genre = ? ";
+                result = jdbcTemplate.query(sql, new Object[] { value }, new SongRowMapper());
+                break;
+            case "title":
+                sql = "SELECT * FROM song WHERE title = ? ";
+                result = jdbcTemplate.query(sql, new Object[] { value }, new SongRowMapper());
+                break;
+        }
+
+        return result;
+    }
+
 }
